c1db4088977fa32cb2e459770b0798734bf82ea0
jcarr98 <jeffrey.carr98@gmail.com>
2018-10-30 12:17:48 -0400

    Removed test comments

--- src/atm.c
+++ src/atm.c
@@ -14,9 +14,6 @@
 // partial writes. If there was an error it returns ERR_PIPE_WRITE_ERR.
 // Note: data is void * since the actual type being written does not matter.
 
-
-// this is a test!
-// hopefully this updates
 static int checked_write(int fd, void *data, int n)
 {
   char *d = (char *)data;
